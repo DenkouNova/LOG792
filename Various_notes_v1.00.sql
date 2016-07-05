2069ca365b8095a200088cbd0609611b5541ed4a
fggallant <fggallant@gmail.com>
2016-07-05 14:35:22 -0400

    Redoing the repository, part 1

---
+++ Various_notes_v1.00.sql
@@ -0,0 +1,24 @@
+
+-- =============================================================================
+-- Pas un vrai script SQL, c'est juste pour avoir des couleurs dans notepad++
+-- 
+-- Historique
+-- v1.00 - ajouté notes sur l'installation de NuGet, NHibernate etc.
+-- =============================================================================
+
+--2016-12-06 11h05
+--Setting up NHibernate blog post
+http://coding-journal.com/setting-up-nhibernate-with-sqlite-using-visual-studio-2010-and-nuget/
+
+--NuGet installer
+https://visualstudiogallery.msdn.microsoft.com/4ec1526c-4a8c-4a84-b702-b21a8f5293ca
+
+--NHibernate Mapping Generator
+https://nmg.codeplex.com/
+
+--NHibernate - Relational Persistence for Idiomatic .NET
+http://nhibernate.info/doc/nh/en/index.html
+
+--NHibernate Mappings for Composite Keys with Associations
+http://www.codeproject.com/Tips/419780/NHibernate-Mappings-for-Composite-Keys-with-Associ
+
---
+++ Various_notes_v1.01.sql
@@ -0,0 +1,30 @@
+
+-- =============================================================================
+--
+-- Pas un vrai script SQL, c'est juste pour avoir des couleurs dans notepad++
+--
+-- =============================================================================
+
+--2016-12-06 11h05
+--Setting up NHibernate blog post
+http://coding-journal.com/setting-up-nhibernate-with-sqlite-using-visual-studio-2010-and-nuget/
+
+--NuGet installer
+https://visualstudiogallery.msdn.microsoft.com/4ec1526c-4a8c-4a84-b702-b21a8f5293ca
+
+--NHibernate Mapping Generator
+https://nmg.codeplex.com/
+
+--NHibernate - Relational Persistence for Idiomatic .NET
+http://nhibernate.info/doc/nh/en/index.html
+
+--NHibernate Mappings for Composite Keys with Associations
+http://www.codeproject.com/Tips/419780/NHibernate-Mappings-for-Composite-Keys-with-Associ
+
+
+--2016-07-04 10h56
+--MVC example Model-View-Controller
+http://www.codeproject.com/Articles/25057/Simple-Example-of-MVC-Model-View-Controller-Design
+
+--Set selected tab font to bold
+http://www.vbforums.com/showthread.php?355093-Setting-selected-tabpage-header-text-to-bold
